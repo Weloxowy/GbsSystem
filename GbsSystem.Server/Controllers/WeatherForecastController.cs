aa8c23bc28d591082b7769c9afb8b2e28fd78052
Mateusz Ziara <ziaramateusz@gmail.com>
2024-10-05 11:39:01 +0200

    test

--- GbsSystem.Server/Controllers/WeatherForecastController.cs
+++ GbsSystem.Server/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace GbsSystem.Server.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mld", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
