706a3b78f4dd5da23b799e798ce8789401a0ddc0
Paweł <karpawell@gmail.com>
2024-10-05 16:45:43 +0200

    Main page

--- gbssystem.client/src/App.tsx
+++ gbssystem.client/src/App.tsx
@@ -1,24 +1,20 @@
-import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import '@mantine/core/styles.css';
-import Register from "./login/Register";
 import MainPage from "./mainPage/MainPage";
 import {  MantineProvider } from '@mantine/core';
 import LoginPage from "./routes/LoginPage/LoginPage.tsx";
+import {theme} from "./theme.tsx";
 
 export default function App() {
 
   return (
-    <MantineProvider>
+    <MantineProvider theme={theme}>
       <BrowserRouter>
         <Routes>
         <Route path="/" element={<MainPage />}>
             {" "}
           </Route>
           <Route path="/login" element={<LoginPage />}>
-            {" "}
-          </Route>
-          <Route path="/register" element={<Register />}>
             {" "}
           </Route>
         </Routes>
