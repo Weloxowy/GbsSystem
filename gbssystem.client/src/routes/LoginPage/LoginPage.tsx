625620e97c7e99e2b159635c4cb250c1c18d5e9b
Przemek <przemyslaw.mlynarczyk1@gmail.com>
2024-10-05 15:20:57 +0200

    Wygasanie zdjęć

--- gbssystem.client/src/routes/LoginPage/LoginPage.tsx
+++ gbssystem.client/src/routes/LoginPage/LoginPage.tsx
@@ -1,5 +1,9 @@
+import { useState, useEffect } from 'react';
 import styles from './LoginPage.module.css'; // importuj plik CSS
 import backgroundImage from '../../assets/Zdj_hackaton_1.jpeg';
+import backgroundImage1 from '../../assets/Zdj_hackaton_2.jpeg';
+import backgroundImage2 from '../../assets/Zdj_hackaton_3.jpeg';
+
 import { useToggle, upperFirst } from '@mantine/hooks';
 import { useForm } from '@mantine/form';
 import {
@@ -18,6 +22,10 @@ import {
 
 export default function LoginPage(props: PaperProps) {
     const [type, toggle] = useToggle(['login', 'register']);
+    const [currentImage, setCurrentImage] = useState(0);
+    const [fade, setFade] = useState(true);
+    const images = [backgroundImage, backgroundImage1, backgroundImage2];
+
     const form = useForm({
         initialValues: {
             email: '',
@@ -32,72 +40,91 @@ export default function LoginPage(props: PaperProps) {
         },
     });
 
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFade(false);
+            setTimeout(() => {
+                setCurrentImage((prevImage) => (prevImage + 1) % images.length);
+                setFade(true);
+            }, 1000);
+        }, 5000);
+
+        return () => clearInterval(interval);
+    }, [images.length]);
+
     return (
-        <div className={styles.loginPage} style={{ backgroundImage: `url(${backgroundImage})` }}>
-            <Paper
-                radius="md"
-                p="xl"
-                withBorder
-                {...props}
-                style={{ width: '30vw', maxWidth: '400px', padding: '40px' }} // Ustalona szerokość w proporcji do ekranu
-            >
-                <Text size="lg" fw={500}>
-                    Welcome to GbsSystem, {type} with
-                </Text>
-                <Divider label="Or continue with email" labelPosition="center" my="lg" />
-                <form onSubmit={form.onSubmit(() => {})}>
-                    <Stack>
-                        {type === 'register' && (
+        <div className={styles.pageContainer}>
+
+            <div className={`${styles.background} ${fade ? styles['fade-in'] : styles['fade-out']}`} style={{ backgroundImage: `url(${images[currentImage]})` }} />
+
+
+            <div className={styles.loginContainer}>
+                <Paper
+                    radius="md"
+                    p="xl"
+                    withBorder
+                    {...props}
+                    style={{ width: '30vw', maxWidth: '400px', padding: '40px' }}
+                >
+                    <Text size="lg" fw={500} style={{textAlign: 'center'}}>
+                        Welcome to GbsSystem
+                    </Text>
+                    <Divider label="Continue with email" labelPosition="center" my="lg" />
+                    <form onSubmit={form.onSubmit(() => {})}>
+                        <Stack>
+                            {type === 'register' && (
+                                <TextInput
+                                    label="Name"
+                                    placeholder="Your name"
+                                    value={form.values.name}
+                                    onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+                                    radius="md"
+                                />
+                            )}
+
                             <TextInput
-                                label="Name"
-                                placeholder="Your name"
-                                value={form.values.name}
-                                onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+                                required
+                                label="Email"
+                                placeholder="hello@mantine.dev"
+                                value={form.values.email}
+                                onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+                                error={form.errors.email && 'Invalid email'}
                                 radius="md"
                             />
-                        )}
 
-                        <TextInput
-                            required
-                            label="Email"
-                            placeholder="hello@mantine.dev"
-                            value={form.values.email}
-                            onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
-                            error={form.errors.email && 'Invalid email'}
-                            radius="md"
-                        />
-
-                        <PasswordInput
-                            required
-                            label="Password"
-                            placeholder="Your password"
-                            value={form.values.password}
-                            onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
-                            error={form.errors.password && 'Password should include at least 6 characters'}
-                            radius="md"
-                        />
-
-                        {type === 'register' && (
-                            <Checkbox
-                                label="I accept terms and conditions"
-                                checked={form.values.terms}
-                                onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+                            <PasswordInput
+                                required
+                                label="Password"
+                                placeholder="Your password"
+                                value={form.values.password}
+                                onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+                                error={form.errors.password && 'Password should include at least 6 characters'}
+                                radius="md"
                             />
-                        )}
-                    </Stack>
 
-                    <Group justify="space-between" mt="xl">
-                        <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
-                            {type === 'register'
-                                ? 'Already have an account? Login'
-                                : "Don't have an account? Register"}
-                        </Anchor>
-                        <Button type="submit" radius="xl">
-                            {upperFirst(type)}
-                        </Button>
-                    </Group>
-                </form>
-            </Paper>
+                            {type === 'register' && (
+                                <Checkbox
+                                    label="I accept terms and conditions"
+                                    checked={form.values.terms}
+                                    onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+                                />
+                            )}
+                        </Stack>
+
+                        <Group justify="space-between" mt="xl">
+                            <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
+                                {type === 'register'
+                                    ? 'Already have an account? Login'
+                                    : "Don't have an account? Register"}
+                            </Anchor>
+                            <Button type="submit" radius="xl">
+                                {upperFirst(type)}
+                            </Button>
+                        </Group>
+                    </form>
+                </Paper>
+            </div>
         </div>
     );
 }
