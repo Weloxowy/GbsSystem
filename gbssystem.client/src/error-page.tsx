63f21b8be4323b63aa25717d1b75e416d0e183b5
Paweł <karpawell@gmail.com>
2024-10-05 12:10:27 +0200

    React router

---
+++ gbssystem.client/src/error-page.tsx
@@ -0,0 +1,24 @@
+import { useRouteError } from "react-router-dom";
+
+export default function ErrorPage() {
+    const error = useRouteError();
+    console.error(error);
+
+    return (
+        <div style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            height: "100vh",
+            width: "100vw"
+        }}>
+            <h1>Oops!</h1>
+            <p>Sorry, an unexpected error has occurred.</p>
+            <p>
+                <i>{error.statusText || error.message}</i>
+            </p>
+        </div>
+
+    );
+}
